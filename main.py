ae8bff8f8193fb2a7fd1b5f3794f8168c256e238
Jeel <jeelsavaliya888@gmail.com>
2026-09-05 15:01:15 +0530

    Basic Boiler Created for Bank

---
+++ main.py
@@ -0,0 +1,11 @@
+print("=" * 50)
+print("                 SAVALIYA ONE")
+print("                   CLI BANK")
+print("=" * 50)
+
+print("\n1. Create Account")
+print("2. Login")
+print("3. Exit")
+
+choice = input("\nEnter your choice: ")
+print("\nYou selected:", choice)
