f017d675d9795f8dc08dd30323a51801b0e9ad60
Jeel <jeelsavaliya888@gmail.com>
2026-09-05 15:18:40 +0530

    Accounts Creation Added

---
+++ account.py
@@ -0,0 +1,9 @@
+def create_account():
+    print("\n-------Create Account-------")
+    name=input("\nEnter your name: ")
+    age=int(input("Enter your age: "))
+    deposit = float(input("Enter initial deposit: ₹"))
+    pin = input("Create a 4-digit PIN: ")
+
+    print("\n✅ Account created successfully!")
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from account import create_account
+
 print("=" * 50)
 print("                 SAVALIYA ONE")
 print("                   CLI BANK")
@@ -9,3 +11,14 @@ print("3. Exit")
 
 choice = input("\nEnter your choice: ")
 print("\nYou selected:", choice)
+
+if(choice=="1"):
+    create_account()
+
+elif(choice=="2"):
+    print("\n--- LOGIN ---")
+    print("Login selected.")
+elif(choice=="3"):
+    print("\nThanks for choosing SAVALIYA ONE :)")
+else:
+    print("\n❌ Invalid choice.")
